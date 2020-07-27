4a7f558b1a95aba68393d0e776fc7d0eb6064d5f
Dmitry Rastoropov <dmitry.rastoropov@gmail.com>
2020-07-27 22:02:57 +0300

    update

--- script-ps.ps1
+++ script-ps.ps1
@@ -1,5 +1,35 @@
 Write-Host "Script started... "
 
-start-service filebeat
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
 
-Write-Host "Finished!"
+#whoami /groups /fo csv | convertfrom-csv | where-object { $_.SID -eq "S-1-5-32-544" }
+
+#$elevated = ([Security.Principal.WindowsPrincipal] `
+# [Security.Principal.WindowsIdentity]::GetCurrent()
+#).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+#write-host $elevated
+
+
+#$output = whoami /all
+#$IsAdministrator = $false
+#foreach($line in $output) {
+#  if ($line -like "*BUILTIN\Administrators*") {
+#      $IsAdministrator = $true
+#      break;
+#   } 
+#} 
+#if ($IsAdministrator)
+#{
+#    Write-Host "The Computer contains Adminstrator priveledges" -ForegroundColor Black -BackgroundColor Green
+#} else {
+#    Write-Host "The Computer does not have Adminstrator priveledges" -ForegroundColor -BackgroundColor Red
+#}
+
+
+#start-service filebeat
+#Write-Host "Finished!"
